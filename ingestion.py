132075b31506c6bd4ae2552e601cc54be224338d
l3bron4 <140657537+l3bron4@users.noreply.github.com>
2026-03-25 11:10:29 +0100

    Update ingestion.py

--- ingestion.py
+++ ingestion.py
@@ -6,7 +6,7 @@ from google.oauth2 import service_account
 
 # 1. CONFIGURATION DES VARIABLES
 # Modifie ces noms selon tes besoins
-PROJECT_ID = "dataflashcards"
+PROJECT_ID = "my-project-florent-bq"
 DATASET_ID = "raw_data_dataflashcards"
 TABLE_ID = "analytics_promo_raw"
 COLLECTION_NAME = "analytics_promo"
